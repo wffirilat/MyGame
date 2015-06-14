992cbf87d408baa620c527724747db3962d15ee3
wffirilat <xwffirilat@gmail.com>
2015-06-14 17:17:40 -0500

    Added @Mod annotation to register a classs as a Mod. Improved mod support.

--- mods/mod_CombatAnnouncer.py
+++ mods/mod_CombatAnnouncer.py
@@ -1,3 +1,5 @@
+from ModLoader import Mod
+
 __author__ = 'xwffirilat'
 
 from event.Events import EventHandler, Priority
@@ -6,13 +8,16 @@ from event.EventLib import EntityAttackedEvent, EntityAttackEvent
 import MainRegistry
 
 
-@EventHandler(EntityAttackedEvent, priority=Priority.NORMAL)
-def alertAttack(event: EntityAttackedEvent):
-    MainRegistry.getGUI().console.add('%s was attacked by %s for %s damage' %
-                                      (event.target, event.source, event.damage))
+@Mod(modid='CombatAnnouncer')
+class CombatAnnouncer:
+    @staticmethod
+    @EventHandler()
+    def alertAttack(event: EntityAttackedEvent):
+        MainRegistry.getGUI().console.add('%s was attacked by %s for %s damage' %
+                                          (event.target, event.source, event.damage))
 
-
-@EventHandler(EntityAttackEvent)
-def alertSwing(event: EntityAttackEvent):
-    MainRegistry.getGUI().console.add('%s (%s) swings at %s (%s)!' %
-                                      (event.attacker, event.atkRoll, event.target, event.defRoll))
+    @staticmethod
+    @EventHandler()
+    def alertSwing(event: EntityAttackEvent):
+        MainRegistry.getGUI().console.add('%s (%s) swings at %s (%s)!' %
+                                          (event.attacker, event.atkRoll, event.target, event.defRoll))
--- mods/mod_MonsterInformer.py
+++ mods/mod_MonsterInformer.py
@@ -1,3 +1,5 @@
+from ModLoader import Mod
+
 __author__ = 'xwffirilat'
 
 from event.Events import EventHandler
@@ -6,16 +8,18 @@ from event.EventLib import MonsterEvent
 import MainRegistry
 
 
-@EventHandler()
-def preAlert(event: MonsterEvent.Pre):
-    MainRegistry.getGUI().console.add("creating Monster:\n"
-                                      "    Size: {}\n    Type: {}\n    Name: {}"
-                                      .format(event.size, event.type, event.name))
+@Mod(modid='MonsterInformer')
+class MonsterInformer:
+    @staticmethod
+    @EventHandler()
+    def preAlert(event: MonsterEvent.Pre):
+        MainRegistry.getGUI().console.add("creating Monster:\n"
+                                          "    Size: {}\n    Type: {}\n    Name: {}"
+                                          .format(event.size, event.type, event.name))
 
-
-@EventHandler()
-def postAlert(event: MonsterEvent.Post):
-    event.monster._health = 1
-    MainRegistry.getGUI().console.add("Finished monster:\n"
-                                      "    Level: {}\n    HP: {}"
-                                      .format(event.monster.level, event.monster.health))
+    @staticmethod
+    @EventHandler()
+    def postAlert(event: MonsterEvent.Post):
+        MainRegistry.getGUI().console.add("Finished monster:\n"
+                                          "    Level: {}\n    HP: {}"
+                                          .format(event.monster.level, event.monster.health))
